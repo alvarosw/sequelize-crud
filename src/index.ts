ded7ef845c2c48b5652c38780fcc39a3d88f5bfd
Álvaro <alvarossmiguel@gmail.com>
2022-09-11 11:19:01 -0300

    feat: server setup

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+require('dotenv').config()
+
+import { start } from './server/server'
+
+start()
---
+++ src/server/server.ts
@@ -0,0 +1,24 @@
+import { Server, IncomingMessage, ServerResponse } from 'http'
+import express from 'express'
+const app = express()
+
+const port = process.env.PORT
+const apiVersion = process.env.API_VERSION
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+let server: Server<typeof IncomingMessage, typeof ServerResponse>
+
+export function start() {
+  if (!server) server = app.listen(port)
+
+  console.debug(`
+    Server is up running on port ${port}
+      - App url: http://localhost:${port}/api/${apiVersion}
+  `)
+}
+
+export function stop() {
+  if (server.listening) server.close()
+}
